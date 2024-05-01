8007e20db85ac48f6aaeb567538e52b5dda4de62
Max <maxmilian.janda@gmail.com>
2024-05-02 00:40:31 +0200

    Další den

---
+++ index.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById("copy-btn");
+const wrap = document.getElementById("click");
+const beforeBtn = document.getElementById("before-click");
+
+btn.addEventListener("click", () => {
+  const textToCopy = "maxmilian.janda@gmail.com"; // ZmÄ›Ĺte na text, kterĂ˝ chcete zkopĂ­rovat
+
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      doSomething();
+    })
+    .catch((error) => {
+      console.error("NepodaĹ™ilo se zkopĂ­rovat text: ", error);
+      alert("Jejda něco se nepodařilo");
+    });
+
+  wrap.style.display = "flex";
+  beforeBtn.style.display = "none";
+  btn.style.backgroundColor = "rgba(15, 138, 81, 0.21)";
+
+  function doSomething() {
+    setTimeout(function () {
+      wrap.style.display = "none";
+      beforeBtn.style.display = "flex";
+      btn.style.backgroundColor = "transparent";
+    }, 2500);
+  }
+});
