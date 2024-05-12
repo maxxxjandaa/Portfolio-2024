09cbc3ba8a78c025b310d1d96ba11948d550ecc1
Max <maxmilian.janda@gmail.com>
2024-05-12 22:56:39 +0200

    Res end

--- js/index.js
+++ js/index.js
@@ -8,23 +8,16 @@ const priceMoreBtn = document.getElementById("price-more-btn");
 
 const firstText = document.querySelector(".change-text");
 const secondText = document.querySelector("#fun02");
-let i = 0;
-secondText.style.opacity = 0;
 
-setInterval(() => {
-  changeText();
-}, 3000);
+// Mobile
 
-function changeText() {
-  if (i == 0) {
-    firstText.style.opacity = 1;
-    i++;
-  } else if (i == 1) {
-    firstText.style.opacity = 0;
-    secondText.style.opacity = 1;
-    i = 0;
-  }
-}
+const mobileNavBtn = document.getElementById("btn-mobile-navBtn");
+const navMobile = document.querySelector(".header-nav");
+const ulNav = document.querySelector(".mobile-nav-ul");
+const menuIconOpen = document.querySelector(".menu-icon-open");
+const menuIconClose = document.querySelector(".menu-icon-close");
+
+const themeBtnMobile = document.getElementById("btn-mobile-theme");
 
 function navigateTo(url) {
   // Přidejte třídu na body, která spustí animaci fade-out
@@ -67,5 +60,17 @@ themeBtn.addEventListener("click", () => {
 });
 
 priceMoreBtn.addEventListener("click", () => {
-  window - alert("Upssss tato funkce není zatím dostupná");
+  window.alert("Upssss tato funkce není zatím dostupná");
+});
+
+// Mobile section
+
+mobileNavBtn.addEventListener("click", () => {
+  navMobile.classList.toggle("showNavMobile");
+  menuIconClose.classList.toggle("menuiconclose");
+  menuIconOpen.classList.toggle("menuiconopen");
+});
+
+themeBtnMobile.addEventListener("click", () => {
+  window.alert("Upssss tato funkce není zatím dostupná");
 });
